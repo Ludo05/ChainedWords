8a4a434ee862e626e48d063c1308be3da6d65c05
Lewis Williams <lewis.williams@transformuk.com>
2019-08-21 10:48:33 +0100

    Tests

--- WordChain.test.js
+++ WordChain.test.js
@@ -2,12 +2,11 @@ const {WordChain} = require('./WordChain');
 
 describe('WordChain',  () => {
 
+    const list = ['dog','cat','tolo']
+
     it('should return a the chained list',  () => {
       const  wordChainTest  = new WordChain('cat','dog','./50kwords.txt');
-     expect(wordChainTest.checkIfWordIsInList()).toBe(true)
-
-
-
+     expect(wordChainTest.checkIfWordIsInList(list,['d','o','g'])).toBe(true)
     });
 
 
